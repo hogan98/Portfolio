6fe30a5d3f4671caa7b264b4e9ccdfafc552007d
Ian <ianreeves19981@gmail.com>
2023-04-25 21:22:36 +0100

    navbar working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,80 @@
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import "../App.css";
-// install react-router-dom
-//install react-icons
-export default function NavBar() {
+import "../styles/App.css";
+import "../styles/Navbar.css";
+
+function NavBar() {
+  const [isNavActive, setIsNavActive] = useState(false);
+
+  const handleClick = () => {
+    setIsNavActive(!isNavActive);
+  };
+
+  const handleNavLinkClick = () => {
+    setIsNavActive(false);
+  };
+
   return (
-    <header>
-      <div className="nav-container">
-        <nav>
-          <ul>
-            <h2>Ian Hogan</h2>
-            <li>
-              <NavLink to="/" activeClassName="active" exact>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/About" activeClassName="active" exact>
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/WebPortfolio" activeClassName="active" exact>
-                WebPortfolio
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Contact" activeClassName="active" exact>
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-          <button className="nav-btn nav-close-btn">
-            <FaTimes />
-          </button>
+    <>
+      <header>
+        <span className="nav-logo">Ian Reeves</span>
+        <div className="nav-toggle" onClick={handleClick}>
+          <FaBars className="bar" />
+        </div>
+        <nav className={`nav-bar ${isNavActive ? "active" : ""}`}>
+          <div className="nav-items">
+            <ul className="links">
+              <li>
+                <NavLink
+                  to="/"
+                  activeClassName="active"
+                  exact
+                  className="nav-link"
+                  onClick={handleNavLinkClick}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/About"
+                  activeClassName="active"
+                  exact
+                  className="nav-link"
+                  onClick={handleNavLinkClick}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/WebPortfolio"
+                  activeClassName="active"
+                  exact
+                  className="nav-link"
+                  onClick={handleNavLinkClick}
+                >
+                  WebPortfolio
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/Contact"
+                  activeClassName="active"
+                  exact
+                  className="nav-link"
+                  onClick={handleNavLinkClick}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
-        <button className="nav-btn">
-          <FaBars />
-        </button>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
+
+export default NavBar;
--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+      itaque, molestias nobis pariatur doloribus quam odit blanditiis.
+      Laboriosam tenetur dolorum harum incidunt maxime quas, aliquam et
+      laudantium natus nihil magnam? Debitis natus fugiat ea accusamus quisquam
+      reprehenderit suscipit. Non facere ex quis perferendis doloribus ut
+      impedit quae dolorum inventore rerum quaerat, nulla quia libero,
+      consectetur maxime iste, aspernatur quidem odio. Itaque quo, reiciendis
+      sunt nulla optio dicta quae. Doloribus recusandae aperiam quisquam officia
+      asperiores. Ullam, quibusdam. Officiis id ea explicabo harum optio vel
+      exercitationem ipsa et, ipsum quas, vitae eius. Praesentium sequi facilis
+      debitis facere, magnam, nulla quibusdam adipisci laboriosam pariatur illum
+      eligendi. Consequuntur expedita itaque quisquam ratione commodi, sapiente
+      dolor voluptates velit, quia laudantium suscipit eius fuga veritatis!
+      Sapiente. Numquam modi quae dicta molestiae animi. Non ad neque officia
+      fuga fugiat amet, consequuntur optio commodi dolore aliquam odio dolores
+      magni, molestiae est, sint vero? Maxime iure quae ex rerum?
+    </div>
+  );
 }
